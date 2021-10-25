0a6fecb2f8c7cd0db9041567daa4bcf66bef7b1e
umairmohd8 <mohammed08@protonmail.com>
2021-10-26 00:25:09 +0530

    code verififcation

--- cogs/verify_mail.py
+++ cogs/verify_mail.py
@@ -7,6 +7,11 @@ from discord.ext import commands
 from private import newDB
 
 BOT_LOG = 846778354157355109
+roles = {'verified': 840568002176614400, 'alum': 902217061692997673, 'first': 842427880675999784,
+         'second': 842427873259946023,
+         'third': 842427880675999784, 'fourth': 842427886324809768, 'cse': 842426829318258728,
+         'ise': 842426832154132534,
+         'ece': 842427172609720341, 'eee': 902216757018763306, 'mech': 902248929222090872, 'civ': 902248689786052659}
 
 
 class Mail(commands.Cog):
@@ -23,8 +28,34 @@ class Mail(commands.Cog):
     def get_code(self):
         passw = random.randint(100000, 999999)
         return passw
-    def assign_roles(self):
 
+    def assign_roles(self, mail):
+        usr_roles = {"year": 0, "branch": 0}
+        if int(mail[3:5]) < 18:
+            usr_roles["year"] = roles['alum']
+        elif int(mail[3:5]) == 18:
+            usr_roles["year"] = roles['fourth']
+        elif int(mail[3:5]) == 19:
+            usr_roles["year"] = roles['third']
+        elif int(mail[3:5]) == 20:
+            usr_roles["year"] = roles['second']
+        elif int(mail[3:5]) == 21:
+            usr_roles["year"] = roles['first']
+
+        if mail[5:7] == 'cs':
+            usr_roles["branch"] = roles['cse']
+        elif mail[5:7] == 'is':
+            usr_roles["branch"] = roles['ise']
+        elif mail[5:7] == 'ec':
+            usr_roles["branch"] = roles['ece']
+        elif mail[5:7] == 'ee':
+            usr_roles["branch"] = roles['eee']
+        elif mail[5:7] == 'me':
+            usr_roles["branch"] = roles['mech']
+        elif mail[5:7] == 'cv':
+            usr_roles["branch"] = roles['civ']
+
+        return usr_roles
 
     def send_mail(self):
         pass
@@ -81,12 +112,28 @@ class Mail(commands.Cog):
 
     @commands.command()
     async def code(self, ctx, passwd):
-        db_pass = newDB.getPass(ctx.author)
+        user = ctx.author
+        db_pass = newDB.getPass(str(user))  # if user not in db , null error
+        # adding verified role
         if db_pass[0] == passwd:
-            role = discord.utils.get(self.bot.get_guild(ctx.guild.id).roles, id = )
-            # adding verifid role
+            role = discord.utils.get(self.bot.get_guild(ctx.guild.id).roles, id=roles['verified'])
+            await user.add_roles(role)
+            # adding other roles
+            usr_mail = newDB.uMail(str(user))
+            print(usr_mail[0])
+            oth_roles = self.assign_roles(usr_mail[0])
 
+            year_role = discord.utils.get(self.bot.get_guild(ctx.guild.id).roles, id=oth_roles['year'])
+            await user.add_roles(year_role)
 
+            branch = discord.utils.get(self.bot.get_guild(ctx.guild.id).roles, id=oth_roles['branch'])
+            await user.add_roles(branch)
+
+            # updating code verification status in db
+            pass_stat = newDB.vStat(str(user))
+            await self.bot.get_channel(BOT_LOG).send(f'{user} {pass_stat}')
+        else:
+            await ctx.channel.send(f'{user.mention} passcode did not match!')
 
     # command to check if the usn has verified code
     @commands.command()
